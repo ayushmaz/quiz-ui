0f4f899722ee5c2d2d317bc2e29f1d5d44cc19df
Ayush Majumder <majumdershuvu@gmail.com>
2024-09-09 12:03:13 +0530

    withCredentials is sent along all apis

--- src/service/api.js
+++ src/service/api.js
@@ -1,13 +1,13 @@
 import axiosInstance from "./axiosInstance"
 
 export const API_GET_QUESTIONS = () => {
-    return axiosInstance.get('/questions');
+    return axiosInstance.get('/questions', {withCredentials: true});
 }
 
 export const API_POST_ANSWER = (id, payload) => {
-    return axiosInstance.post(`/question/${id}/answer`, payload);
+    return axiosInstance.post(`/question/${id}/answer`, payload, {withCredentials: true});
 }
 
 export const API_SUBMIT_QUIZ = () => {
-    return axiosInstance.post(`/submit`);
+    return axiosInstance.post(`/submit`, null, {withCredentials: true});
 }
